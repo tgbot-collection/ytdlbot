a215bcf245067f194582608265bd28bfe8a40ce6
BennyThink <benny.think@gmail.com>
2022-03-09 21:24:36 +0800

    enable IPv6

--- ytdlbot/client_init.py
+++ ytdlbot/client_init.py
@@ -7,6 +7,8 @@
 
 __author__ = "Benny <benny.think@gmail.com>"
 
+import os
+
 from pyrogram import Client
 
 from config import APP_HASH, APP_ID, PYRO_WORKERS, TOKEN
@@ -15,6 +17,7 @@ from config import APP_HASH, APP_ID, PYRO_WORKERS, TOKEN
 def create_app(session="ytdl", workers=PYRO_WORKERS):
     _app = Client(session, APP_ID, APP_HASH,
                   bot_token=TOKEN, workers=workers,
+                  ipv6=os.getenv("ipv6", False),
                   # proxy={"hostname": "host.docker.internal", "port": 1080}
                   )
 
