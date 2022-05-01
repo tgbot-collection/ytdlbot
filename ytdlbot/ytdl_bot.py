5b581fc37d7f6b757fd033fac8ef905b2ef8bdbf
BennyThink <benny.think@gmail.com>
2022-05-01 12:18:13 +0800

    sub with 3600s

--- ytdlbot/ytdl_bot.py
+++ ytdlbot/ytdl_bot.py
@@ -359,7 +359,7 @@ if __name__ == '__main__':
     scheduler.add_job(auto_restart, 'interval', seconds=5)
     scheduler.add_job(InfluxDB().collect_data, 'interval', seconds=60)
     #  default quota allocation of 10,000 units per day,
-    # scheduler.add_job(periodic_sub_check, 'interval', seconds=60 * 30)
+    scheduler.add_job(periodic_sub_check, 'interval', seconds=60 * 60)
     scheduler.start()
     banner = f"""
 ▌ ▌         ▀▛▘     ▌       ▛▀▖              ▜            ▌
