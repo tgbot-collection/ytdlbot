455cad608018af4b681572bd591b809ccf88307a
Benny <benny.think@gmail.com>
2023-04-08 13:34:54 +0200

    bad config REQUIRED_MEMBERSHIP

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -35,7 +35,7 @@ OWNER = os.getenv("OWNER", "BennyThink")
 AUTHORIZED_USER: "str" = os.getenv("AUTHORIZED_USER", "")
 # membership requires: the format could be username(without @ sign)/chat_id of channel or group.
 # You need to add the bot to this group/channel as admin
-REQUIRED_MEMBERSHIP: "str" = os.getenv("REQUIRED_MEMBERSHIP", "mikuri520")
+REQUIRED_MEMBERSHIP: "str" = os.getenv("REQUIRED_MEMBERSHIP", "")
 
 # celery related
 ENABLE_CELERY = os.getenv("ENABLE_CELERY", False)
