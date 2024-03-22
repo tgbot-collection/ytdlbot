1ef601e607e2178933503f2ae1401f9700cdf564
SanujaNS <sanuja.senaviratne@gmail.com>
2024-03-22 19:05:01 +0530

    sp_ytdl_download: remove aria2 and clean up

--- ytdlbot/sp_downloader.py
+++ ytdlbot/sp_downloader.py
@@ -20,7 +20,6 @@ import filetype
 import yt_dlp as ytdl
 
 from config import (
-    ENABLE_ARIA2,
     IPv6,
 )
 
@@ -55,41 +54,23 @@ def sp_ytdl_download(url: str, tempdir: str):
         "outtmpl": output,
         "restrictfilenames": False,
         "quiet": True,
+        "format": None,
     }
-    if ENABLE_ARIA2:
-        ydl_opts["external_downloader"] = "aria2c"
-        ydl_opts["external_downloader_args"] = [
-            "--min-split-size=1M",
-            "--max-connection-per-server=16",
-            "--max-concurrent-downloads=16",
-            "--split=16",
-        ]
-    formats = [
-        # webm , vp9 and av01 are not streamable on telegram, so we'll extract mp4 and not av01 codec
-        "bestvideo[ext=mp4][vcodec!*=av01][vcodec!*=vp09]+bestaudio[ext=m4a]/bestvideo+bestaudio",
-        "bestvideo[vcodec^=avc]+bestaudio[acodec^=mp4a]/best[vcodec^=avc]/best",
-        None,
-    ]
 
     address = ["::", "0.0.0.0"] if IPv6 else [None]
     error = None
     video_paths = None
-    for format_ in formats:
-        ydl_opts["format"] = format_
-        for addr in address:
-            # IPv6 goes first in each format
-            ydl_opts["source_address"] = addr
-            try:
-                logging.info("Downloading for %s with format %s", url, format_)
-                with ytdl.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([url])
-                video_paths = list(pathlib.Path(tempdir).glob("*"))
-                break
-            except Exception:
-                error = traceback.format_exc()
-                logging.error("Download failed for %s - %s, try another way", format_, url)
-        if error is None:
+    for addr in address:
+        ydl_opts["source_address"] = addr
+        try:
+            logging.info("Downloading %s", url)
+            with ytdl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            video_paths = list(pathlib.Path(tempdir).glob("*"))
             break
+        except Exception:
+            error = traceback.format_exc()
+            logging.error("Download failed for %s - %s", url)
 
     if not video_paths:
         raise Exception(error)
