d7b8aaa0b33cc1487f4ecbf1cb12465b1373a9b5
SanujaNS <sanuja.senaviratne@gmail.com>
2024-05-06 22:55:28 +0530

    reformat

--- ytdlbot/sp_downloader.py
+++ ytdlbot/sp_downloader.py
@@ -153,25 +153,25 @@ def pixeldrain(url: str, tempdir: str, bm, **kwargs):
 def krakenfiles(url: str, tempdir: str, bm, **kwargs):
     resp = requests.get(url)
     html = resp.content
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
     link_parts = []
     token_parts = []
-    for form_tag in soup.find_all('form'):
-        action = form_tag.get('action')
-        if action and 'krakenfiles.com' in action:
+    for form_tag in soup.find_all("form"):
+        action = form_tag.get("action")
+        if action and "krakenfiles.com" in action:
             link_parts.append(action)
-        input_tag = form_tag.find('input', {'name': 'token'})
+        input_tag = form_tag.find("input", {"name": "token"})
         if input_tag:
-            value = input_tag.get('value')
+            value = input_tag.get("value")
             token_parts.append(value)
     for link_part, token_part in zip(link_parts, token_parts):
-        link = f'https:{link_part}'
+        link = f"https:{link_part}"
         data = {
-            'token': token_part
+            "token": token_part
         }
         response = requests.post(link, data=data)
         json_data = response.json()
-        url = json_data['url']
+        url = json_data["url"]
     return sp_ytdl_download(url, tempdir, bm, **kwargs)
 
 
@@ -179,25 +179,25 @@ def find_between(s, start, end):
     return (s.split(start))[1].split(end)[0]
 
 def terabox(url: str, tempdir: str, bm, **kwargs):
-    cookies_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'terabox.txt')
+    cookies_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "terabox.txt")
     cookies = parse_cookie_file(cookies_file)
     
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9,hi;q=0.8',
-        'Connection': 'keep-alive',
-        'DNT': '1',
-        'Host': 'www.terabox.app',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
+        "Connection": "keep-alive",
+        "DNT": "1",
+        "Host": "www.terabox.app",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+        "sec-ch-ua": "'Not A(Brand';v='99', 'Google Chrome';v='121', 'Chromium';v='121'",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "'Windows'",
     }
     
     session = requests.Session()
@@ -205,7 +205,7 @@ def terabox(url: str, tempdir: str, bm, **kwargs):
     session.cookies.update(cookies)
     temp_req = session.get(url)
     request_url = urlparse(temp_req.url)
-    surl = parse_qs(request_url.query).get('surl')
+    surl = parse_qs(request_url.query).get("surl")
     req = session.get(temp_req.url)
     respo = req.text
     js_token = find_between(respo, "fn%28%22", "%22%29")
@@ -213,32 +213,32 @@ def terabox(url: str, tempdir: str, bm, **kwargs):
     bdstoken = find_between(respo, 'bdstoken":"', '"')
     
     params = {
-        'app_id': '250528',
-        'web': '1',
-        'channel': 'dubox',
-        'clienttype': '0',
-        'jsToken': js_token,
-        'dp-logid': logid,
-        'page': '1',
-        'num': '20',
-        'by': 'name',
-        'order': 'asc',
-        'site_referer': temp_req.url,
-        'shorturl': surl,
-        'root': '1,',
+        "app_id": "250528",
+        "web": "1",
+        "channel": "dubox",
+        "clienttype": "0",
+        "jsToken": js_token,
+        "dp-logid": logid,
+        "page": "1",
+        "num": "20",
+        "by": "name",
+        "order": "asc",
+        "site_referer": temp_req.url,
+        "shorturl": surl,
+        "root": "1,",
     }
     
-    req2 = session.get('https://www.terabox.app/share/list', params=params)
+    req2 = session.get("https://www.terabox.app/share/list", params=params)
     response_data2 = req2.json()
-    file_name = response_data2['list'][0]['server_filename']
-    sizebytes = int(response_data2['list'][0]['size'])
+    file_name = response_data2["list"][0]["server_filename"]
+    sizebytes = int(response_data2["list"][0]["size"])
     if sizebytes > 48 * 1024 * 1024:
-        direct_link = response_data2['list'][0]['dlink']
-        url = direct_link.replace('d.terabox.app', 'd3.terabox.app')
+        direct_link = response_data2["list"][0]["dlink"]
+        url = direct_link.replace("d.terabox.app", "d3.terabox.app")
     else:
-        direct_link_response = session.head(response_data2['list'][0]['dlink'])
+        direct_link_response = session.head(response_data2["list"][0]["dlink"])
         direct_link_response_headers = direct_link_response.headers
-        direct_link = direct_link_response_headers['Location']
+        direct_link = direct_link_response_headers["Location"]
         url = direct_link
     
     return sp_ytdl_download(url, tempdir, bm, filename=file_name, **kwargs)
