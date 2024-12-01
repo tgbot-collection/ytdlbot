83a8c739ef0ecd1fbf9bb8950a9f965379cc38c2
Benny <benny.think@gmail.com>
2024-12-01 17:53:32 +0100

    pre-commit

---
+++ pre-commit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# ytdlbot - pre-commit.py
+# for dependabot
+import tomllib
+import subprocess
+
+with open("pyproject.toml", "rb") as file:
+    config = tomllib.load(file)
+
+
+with open("requirements.txt", "w") as file:
+    for item in config["project"]["dependencies"]:
+        if " " in item:
+            item = item.split()[-1]
+        file.write(f"{item}\n")
+
+# commit with ammend
+subprocess.run(["git", "add", "requirements.txt"])
+subprocess.run(["git", "commit", "--amend", "--no-edit"])
